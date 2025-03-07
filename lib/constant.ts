3bd972b7781d0ec4d32d4c95069e563d0dde9f8d
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2025-03-07 15:52:03 +0700

    02-create-website-layout

---
+++ lib/constant.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Amazone Clone";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Belanja banyak, hati senang.";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Belajar NextJS dengan menggunakan React, TypeScript, TailwindCSS, NextAuth, Prisma, Vercel, and more.";
---
+++ lib/data.ts
@@ -0,0 +1,38 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deals",
+      href: "/search?tag=todays-deals",
+    },
+    {
+      name: "Best Sellers",
+      href: "/search?tag=best-sellers",
+    },
+    {
+      name: "New Releases",
+      href: "/search?tag=new-releases",
+    },
+    {
+      name: "Electronics",
+      href: "/search?tag=electronics",
+    },
+    {
+      name: "Clothing",
+      href: "/search?tag=clothing",
+    },
+    {
+      name: "Home & Kitchen",
+      href: "/search?tag=home-and-kitchen",
+    },
+    {
+      name: "Beauty & Personal Care",
+      href: "/search?tag=beauty-and-personal-care",
+    },
+    {
+      name: "Food & Grocery",
+      href: "/search?tag=food-and-grocery",
+    },
+  ],
+};
+
+export default data;
