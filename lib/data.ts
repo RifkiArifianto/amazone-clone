9287334e8866a6fbe98322d817575f9e08a5cb30
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2025-03-08 09:50:14 +0700

    04-create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,29 @@ const data = {
       href: "/search?tag=food-and-grocery",
     },
   ],
+  carousels: [
+    {
+      title: "Best Sellers in T-shirts",
+      buttonCaption: "Shop Now",
+      image: "/images/banner1.jpg",
+      url: "/search?category=T-shirts",
+      isPublished: true,
+    },
+    {
+      title: "Best Sellers in Wrist Watches",
+      buttonCaption: "Shop Now",
+      image: "/images/banner2.jpg",
+      url: "/search?category=Watches",
+      isPublished: true,
+    },
+    {
+      title: "Best Sellers in Shoes",
+      buttonCaption: "Shop Now",
+      image: "/images/banner3.jpg",
+      url: "/search?category=Shoes",
+      isPublished: true,
+    },
+  ],
 };
 
 export default data;
