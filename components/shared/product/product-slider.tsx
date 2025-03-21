1ba53bb9821762928eb1df9a26327c62b9d4fde6
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2025-03-21 23:58:35 +0700

    06-update

--- components/shared/product/product-slider.tsx
+++ components/shared/product/product-slider.tsx
@@ -27,28 +27,29 @@ export default function ProductSlider({
           align: "start",
         }}
         className="w-full"
-      />
-      <CarouselContent>
-        {products.map((product) => (
-          <CarouselItem
-            key={product.slug}
-            className={
-              hideDetails
-                ? "md:basis-1/4 lg:basis-1/6"
-                : "md:basis-1/3 lg:basis-1/5"
-            }
-          >
-            <ProductCard
-              hideDetails={hideDetails}
-              hideAddToCart
-              hideBorder
-              product={product}
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="left-0" />
-      <CarouselNext className="right-0" />
+      >
+        <CarouselContent>
+          {products.map((product) => (
+            <CarouselItem
+              key={product.slug}
+              className={
+                hideDetails
+                  ? "md:basis-1/4 lg:basis-1/6"
+                  : "md:basis-1/3 lg:basis-1/5"
+              }
+            >
+              <ProductCard
+                hideDetails={hideDetails}
+                hideAddToCart
+                hideBorder
+                product={product}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-0" />
+        <CarouselNext className="right-0" />
+      </Carousel>
     </div>
   );
 }
